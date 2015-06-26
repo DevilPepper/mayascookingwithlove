507973e03cbc3f284bc5c6429adf6fd28101ff70
Stuff <Stuff@win7>
2015-06-26 11:50:43 -0400

    getting stated

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    new Share(".share-button", { ui: { flyout: 'bottom center' } });
+    // Stick the #nav to the top of the window
+    var nav = $('.navi');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function () {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width(),
+                zIndex: 2
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed) {
+            nav.css({
+                position: 'static',
+                zIndex: 'auto'
+            });
+            isFixed = false;
+        }
+    });
+});
